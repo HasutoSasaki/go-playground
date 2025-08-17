1d497794b146300d1665fb9ca2a665d53129398e
HasutoSasaki <hasuto.sa@gmail.com>
2025-08-17 22:35:46 +0900

    feat(composite): implement demo function to showcase various slice and struct functionalities

---
+++ composite/main.go
@@ -0,0 +1,19 @@
+package composite
+
+func Demo() {
+	Array()
+	SliceNil()
+	SliceAppendBasic()
+	SliceCapacityGrowth()
+	SliceMake()
+	SliceClear()
+	SliceLiteral()
+	SliceExpression()
+	SliceSharing()
+	SliceAppend()
+	SliceCopy()
+	SliceFullExpression()
+	StructBasic()
+	StructAnonymous()
+	Practice()
+}
